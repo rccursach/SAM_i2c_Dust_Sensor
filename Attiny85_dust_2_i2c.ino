3709e8229441a38f3d093967391cac8a09626108
Ricardo <rccursach@gmail.com>
2015-02-14 17:51:28 -0300

    Added led indicator for data collecting status

--- Attiny85_dust_2_i2c.ino
+++ Attiny85_dust_2_i2c.ino
@@ -10,6 +10,14 @@
 
   0x403cc000
   2.9492188 <--- IEEE 754 Float http://www.h-schmidt.net/FloatConverter/IEEE754.html
+  
+                     ----
+  RST             --|°   |--    VIN
+  DUST_READ / A3  --|    |--    SCL
+  DUST_LED  / 4   --|    |--    1 / LED_PIN
+  GND             --|    |--    SDA
+                     ----
+  
 */
 
 
@@ -21,13 +29,17 @@
 #define DELAY_TIME1     28  // Wait 28 us before read sensor value (cf : datasheet)
 #define DELAY_TIME2     10  // Wait 10 ms before reread sensor value (cf : datasheet)
 
-#define SAMPLES         10  // How many samples to take
+#define SAMPLES         40  // How many samples to take
+
+#define LED_PIN         1
 
 int readings[SAMPLES];
 
 void setup(){
   pinMode(DUST_LED,OUTPUT);
   pinMode(DUST_READ,INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
   TinyWireS.begin(I2C_SLAVE_ADDR);      // init I2C Slave mode
 }
 
@@ -37,6 +49,10 @@ void loop(){
     switch(command){
       case 0x00:
         
+        // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+        digitalWrite(LED_PIN, HIGH);
+        // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+        
         // Take [SAMPLE] Times samples and push them to array
         for(int i=0; i<SAMPLES; i++){
           digitalWrite(DUST_LED,LOW);     //++
@@ -45,24 +61,54 @@ void loop(){
           digitalWrite(DUST_LED,HIGH);    //++
           delay(DELAY_TIME2);             //++
         }
+
+
+        //------------------------------------//        
+        //Cutting edges -10 ... 10+ over 40
+        boolean clean = true;        
+        for(int i=0; i<SAMPLES-1; i++){
+          clean = true;
+          for(int j=1; j<SAMPLES; j++){
+            if(readings[i] > readings[j]){
+              int aux = readings[j];
+              readings[j] = readings[i];
+              readings[i] = aux;
+              clean = false;
+            }
+          }
+          if(clean){ break; }
+        }
+
+        float r = 0.0;
+        for(int i=10; i<SAMPLES-10; i++){
+          r += readings[i];
+        }
+        r = r / (SAMPLES-20);
+        
+        //------------------------------------//
         
         // Obtaining Arithmetic Media on variable r
+        /*
         float r = 0.0;
         for(int i=0; i<SAMPLES; i++){
           r += readings[i];
         }
         r = r / SAMPLES;
+        */
         
         // Value to Volts
-        float f = (r * 5.0f)/1024.0;
+        float f = 0.00000000;
+        f = (r * 5.0)/1024.0;
         // Sending float as bytes
         TinyWireS.send( *((uint8_t *)&f + 3) );
         TinyWireS.send( *((uint8_t *)&f + 2) );
         TinyWireS.send( *((uint8_t *)&f + 1) );
         TinyWireS.send( *((uint8_t *)&f)  );
-
-
-
+        
+        // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+        digitalWrite(LED_PIN, LOW);
+        // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+        
       break;
     }
   }
